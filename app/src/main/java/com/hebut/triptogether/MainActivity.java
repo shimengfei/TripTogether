71142382c7e155dbbae39cd96be4d201b10e91b1
shimengfei <38394120+shimengfei@users.noreply.github.com>
2018-05-06 21:23:18 +0800

    Initial commit

---
+++ app/src/main/java/com/hebut/triptogether/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hebut.triptogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ab.activity.AbActivity;
+
+public class MainActivity extends AbActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
